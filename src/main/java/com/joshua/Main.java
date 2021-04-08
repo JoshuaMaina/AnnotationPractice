4ef6c411c37bb79228e437c34dc815100f1ab4d6
Joshua Maina <mmaina073@gmail.com>
2021-04-08 13:47:56 +0300

    Separation of concerns

--- src/main/java/com/joshua/Main.java
+++ src/main/java/com/joshua/Main.java
@@ -1,53 +1,28 @@
 package com.joshua;
 
-import com.joshua.annotations.Column;
 import com.joshua.annotations.Entity;
-import com.joshua.annotations.Id;
-import com.joshua.annotations.Table;
 import com.joshua.entities.DatabaseTable;
+import com.joshua.resources.Mapper;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
 
+// Ps, I know it's not the best code haha. I'm just playing around with concepts right now
 
+//TODO Try and implement a full function that works for an entity
+//TODO Use Reflection to search for the other classes with @Entity annotation
+//TODO Generate multiple 'SQL Statements' for the variety of classes
 public class Main {
     static Map<Class<?>, String> columnType = new HashMap<>(
             Map.of(Integer.class, "int", Double.class, "double"));
 
     public static void main(String[] args) {
         Class<DatabaseTable> cls = DatabaseTable.class;
-        if (cls.isAnnotationPresent(Entity.class))
-            createSQLStatement(cls);
-    }
-
-
-    private static void createSQLStatement(Class<?> cls) {
-        String table_name;
-        if (cls.isAnnotationPresent(Table.class)){
-            table_name = cls.getDeclaredAnnotation(Table.class).name();
-        }else{
-            table_name = cls.getSimpleName().toUpperCase();
+        Mapper mapper = Mapper.getMapper();
+        if (cls.isAnnotationPresent(Entity.class)) {
+            String statement = mapper.getStatement(cls);
+            System.out.println(statement);
         }
-        System.out.println("CREATE TABLE " + table_name);
-        Field[] declaredFields = cls.getDeclaredFields();
-        Arrays.stream(declaredFields)
-                .map(x -> columnNameGenerator.apply(x) + " " + columnTypeGenerator.apply(x))
-                .reduce(fieldAccumulator).ifPresent(System.out::println);
-
     }
-
-    static BinaryOperator<String> fieldAccumulator = (x, y) -> x + "\n" + y;
-
-    static Function<Field, String> columnNameGenerator = field ->
-            (field.isAnnotationPresent(Column.class)
-                ? field.getDeclaredAnnotation(Column.class).name()
-                : field.getName()).toUpperCase();
-
-    static Function<Field, String> columnTypeGenerator = field -> columnType.getOrDefault(field.getType(), "varchar(255)");
-
 
 }
---
+++ src/main/java/com/joshua/resources/Mapper.java
@@ -0,0 +1,85 @@
+package com.joshua.resources;
+
+import com.joshua.annotations.Column;
+import com.joshua.annotations.Id;
+import com.joshua.annotations.Table;
+
+import java.lang.reflect.Field;
+import java.util.StringJoiner;
+import java.util.function.Function;
+
+public interface Mapper {
+    String getStatement(Class<?> cls);
+    String getStatement(Field field);
+    String getStatement(Field[] fields);
+    Field getId(Field[] fields);
+    static Mapper getMapper(){
+        return new MapperImpl();
+    }
+
+    default String fieldName(Field field) {
+        Column columnAnn = field.getDeclaredAnnotation(Column.class);
+        return columnAnn == null
+                ? field.getName()
+                : columnAnn.name();
+    }
+
+    default String fieldType(Field field) {
+        return "varchar(255)";
+    }
+
+    default String fieldArguments(Field field) {
+        return "";
+    }
+
+    class MapperImpl implements Mapper {
+        @Override
+        public String getStatement(Class<?> cls) {
+            Table tableAnn = cls.getDeclaredAnnotation(Table.class);
+            String table_name = tableAnn == null
+                    ? cls.getSimpleName().toUpperCase()
+                    : tableAnn.name().toUpperCase();
+            return String.format("""
+                CREATE TABLE %s (
+                %s
+                PRIMARY KEY(%s))""", table_name, getStatement(cls.getDeclaredFields()),
+                    fieldName(getId(cls.getDeclaredFields())));
+        }
+
+        @Override
+        public String getStatement(Field field) {
+            return String.format("%s %s %s", fieldName(field).toUpperCase(), fieldArguments(field), fieldType(field));
+        }
+
+        @Override
+        public String getStatement(Field[] fields) {
+            if (fields.length == 0) return null;
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Field field : fields)
+                joiner.add(getStatement(field));
+
+            return joiner.toString();
+        }
+
+        @Override
+        public Field getId(Field[] fields) {
+            if (fields.length == 0) return null;
+            Class<?> cls = fields[0].getDeclaringClass();
+            Field primaryKey = null;
+            int count = 0;
+            for (Field field: fields){
+                if (field.isAnnotationPresent(Id.class)){
+                    if (count == 1){
+                        throw new IllegalArgumentException("The class " + cls.getSimpleName() + "has more than one ID");
+                    } else {
+                        primaryKey = field;
+                        count ++;
+                    }
+                }
+            }
+            if (primaryKey == null)
+                throw new IllegalArgumentException("The class " + cls.getSimpleName() + "has no ID");
+            return primaryKey;
+        }
+    }
+}
